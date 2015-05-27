2f85b0cc9e0d239922ad3bad23b8c3f59b56b7bb
ChienliMa <maqianlie@gmail.com>
2015-05-27 20:53:13 +0800

    Modify link.map_storage() so that it make use of given storage_map

--- theano/gof/link.py
+++ theano/gof/link.py
@@ -465,7 +465,7 @@ class Container(object):
         return r
 
 
-def map_storage(fgraph, order, input_storage, output_storage):
+def map_storage(fgraph, order, input_storage, output_storage, storage_map = None):
     """Ensure there is storage (a length-1 list) for inputs, outputs, and interior nodes.
 
     :param fgraph: The current fgraph.  This function uses the inputs and outputs attributes.
@@ -492,8 +492,9 @@ def map_storage(fgraph, order, input_storage, output_storage):
         input_storage = [[None] for input in fgraph.inputs]
     else:
         assert len(fgraph.inputs) == len(input_storage)
-
-    storage_map = {}
+        
+    if storage_map is None:
+        storage_map = {}
     # add input storage into storage_map
     for r, storage in zip(fgraph.inputs, input_storage):
         storage_map[r] = storage
